dc0b92e6228eac5b593bed19082c4ccc4b98755a
kamalsahu0001 <57159499+kamalsahu0001@users.noreply.github.com>
2025-07-21 22:22:58 -0700

    Update test_pfc_pause_lossless_with_snappi.py
    fixing missing imports for reboot cases

--- tests/snappi_tests/pfc/test_pfc_pause_lossless_with_snappi.py
+++ tests/snappi_tests/pfc/test_pfc_pause_lossless_with_snappi.py
@@ -7,7 +7,7 @@ from tests.common.snappi_tests.snappi_fixtures import snappi_api_serv_ip, snappi
     snappi_testbed_config, get_snappi_ports_single_dut, snappi_port_selection, tgen_port_info, \
     get_snappi_ports, is_snappi_multidut                                        # noqa: F401
 from tests.snappi_tests.files.helper import reboot_duts, \
-    reboot_duts_and_disable_wd                                              # noqa: F401
+    setup_ports_and_dut, multidut_port_info, reboot_duts_and_disable_wd                                              # noqa: F401
 from tests.common.snappi_tests.qos_fixtures import prio_dscp_map, all_prio_list, lossless_prio_list,\
     lossy_prio_list, disable_pfcwd          # noqa: F401
 from tests.snappi_tests.pfc.files.helper import run_pfc_test
